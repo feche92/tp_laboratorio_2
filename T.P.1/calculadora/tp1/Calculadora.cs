7e9b26df5831a067da2eb2d8c1e87831390c045b
feche92 <fede_larenga_92@hotmail.com>
2017-09-15 13:47:24 -0300

    tp1

---
+++ T.P.1/calculadora/tp1/Calculadora.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp1
+{
+    class Calculadora
+    {
+        /// <summary>
+        /// realiza la operacion correspondiente con los numeros y el operador que se le pase como parametros
+        /// </summary>
+        /// <param name="numero1">primera instancia de la clase Numero</param>
+        /// <param name="numero2">segunda instancia de la clase Numero</param>
+        /// <param name="operador">string del operador</param>
+        /// <returns>retorna el resultado de dicha operacion</returns>
+        public static double Operar(Numero numero1, Numero numero2, string operador)
+        {
+            double valor = 0;
+            operador = Calculadora.ValidarOperador(operador);
+            switch (operador)
+            {
+                case "+":
+                    valor = numero1.GetNumero() + numero2.GetNumero();
+                    break;
+                case "-":
+                    valor = numero1.GetNumero() - numero2.GetNumero();
+                    break;
+                case "/":
+                    if (numero2.GetNumero() == 0)
+                        valor = 0;
+                    else
+                        valor = numero1.GetNumero() / numero2.GetNumero();
+                    break;
+                case "*":
+                    valor = numero1.GetNumero() * numero2.GetNumero();
+                    break;
+            }
+
+            return valor;
+        }
+        /// <summary>
+        /// valida que el operador que reciba como parametro sea valido
+        /// </summary>
+        /// <param name="operador">string del operador</param>
+        /// <returns>retorna el operador correspondiente si es valido o "+" si es invalido</returns>
+        private static string ValidarOperador(string operador)
+        {
+            string valor = "+";
+            if (operador == "+" || operador == "-" || operador == "/" || operador == "*")
+                valor = operador;
+            return valor;
+        }
+    }
+}
+
---
+++ T.P.1/calculadora/tp1/Numero.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp1
+{
+    class Numero
+    {
+        private double numero;
+        /// <summary>
+        /// retorna el atributo numero del objeto Numero
+        /// </summary>
+        /// <returns></returns>
+        public double GetNumero()
+        {
+            return this.numero;
+        }
+        /// <summary>
+        /// constructor por defecto del objeto Numero
+        /// </summary>
+        public Numero() : this(0)
+        {
+        }
+        /// <summary>
+        /// crea una nueva instancia de la clase Numero
+        /// </summary>
+        /// <param name="numero">le da un valor inicial al atributo numero</param>                
+        public Numero(string numero)
+        {
+            this.SetNumero(numero);
+        }
+        /// <summary>
+        /// crea una nueva instancia de la clase Numero
+        /// </summary>
+        /// <param name="numero">le da un valor inicial al atributo numero</param>
+        public Numero(double numero)
+        {
+            this.numero = numero;
+        }
+        /// <summary>
+        /// valida que el string sea un numero valido
+        /// </summary>
+        /// <param name="numero">string a validar</param>
+        /// <returns>retorna el numero si es valido o 0 si no lo es</returns>
+        private static double ValidarNumero(string numero)
+        {
+            double valorDevuelto = 0;
+            if (double.TryParse(numero, out valorDevuelto))
+                return valorDevuelto;
+            else
+                return valorDevuelto;
+        }
+                    
+        private void SetNumero(string numero)
+        {
+            this.numero = Numero.ValidarNumero(numero);
+        }
+    }
+}
