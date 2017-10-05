70fb7dfe81f9390bf38e342abff6c5be41b121bf
feche92 <fede_larenga_92@hotmail.com>
2017-10-05 13:32:51 -0300

    tp-02

---
+++ TP-02/Entidades/Dulce.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2017
+{
+    public class Dulce : Producto
+    {
+        public Dulce(EMarca marca, string patente, ConsoleColor color):base(patente,marca,color)
+        {
+        }
+        /// <summary>
+        /// Los dulces tienen 80 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 80;
+            }
+        }
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("DULCE\n");
+            sb.AppendFormat(base.Mostrar());
+            sb.AppendFormat("CALORIAS : {0}\r\n", this.CantidadCalorias);
+            sb.AppendFormat("");
+            sb.AppendFormat("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
